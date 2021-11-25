d0d72df86ed6ed22becaab812a07d4f97600d87a
sapytn <sapytn@gmail.com>
2021-11-25 19:55:23 +0300

    join menu

---
+++ dict_menu.py
@@ -0,0 +1,20 @@
+menu = {
+    'salads': {
+        'olivie':45,
+    },
+    'starter':{
+
+    },
+    'second':{
+
+    },
+    'garnish': {
+        'rice':20,
+    },
+    'drink':{
+        'tea':5,
+    },
+    'pies':{
+
+    },
+}
